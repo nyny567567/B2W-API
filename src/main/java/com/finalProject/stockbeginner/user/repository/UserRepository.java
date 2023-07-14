321323f43b6bccd3d27dc9c83a797044864092ec
jaemin <jaemin4033@gmail.com>
2023-07-14 09:59:18 +0900

    before merge

--- src/main/java/com/finalProject/stockbeginner/user/repository/UserRepository.java
+++ src/main/java/com/finalProject/stockbeginner/user/repository/UserRepository.java
@@ -11,7 +11,7 @@ public interface UserRepository extends JpaRepository<User, String> {
    Optional<User> findByEmail(String email);
 
     // 이메일 중복 체크
-       boolean existsByEmail(String email);
+   boolean existsByEmail(String email);
 
 
     // 닉네임 중복 체크
