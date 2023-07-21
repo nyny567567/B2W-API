08b6bafd01299af6c0815166bdc610c57c57e1cf
jaemin <jaemin4033@gmail.com>
2023-07-21 15:25:48 +0900

    test: ranking

---
+++ src/main/java/com/finalProject/stockbeginner/trade/entity/Ranking.java
@@ -0,0 +1,29 @@
+package com.finalProject.stockbeginner.trade.entity;
+
+import com.finalProject.stockbeginner.user.entity.User;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "trade_ranking")
+public class Ranking {
+
+
+
+    private String id;
+
+    private User user;
+
+    private Long profit;
+
+}
