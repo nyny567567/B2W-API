be7c0a552caa76d77ff00305a82554ee0c0d8d8e
ny <ny567@naver.com>
2023-07-25 16:58:01 +0900

    fix: 회원등급중

--- src/main/java/com/finalProject/stockbeginner/StockbeginnerApplication.java
+++ src/main/java/com/finalProject/stockbeginner/StockbeginnerApplication.java
@@ -1,13 +1,21 @@
 package com.finalProject.stockbeginner;
 
+import com.finalProject.stockbeginner.user.entity.User;
+import com.finalProject.stockbeginner.user.entity.UserRole;
+import com.finalProject.stockbeginner.user.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class StockbeginnerApplication {
-
+UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(StockbeginnerApplication.class, args);
 	}
 
-}
+
+	}
+
+
